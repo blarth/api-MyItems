12b9a959c8cced71ff86fb5753e1a01f232f0c3a
blarth <jminocente@gmail.com>
2022-02-09 16:10:21 -0300

    fix: userData undefined in validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,10 @@ import bcrypt from 'bcrypt'
 export async function signUp(req, res) {
   
     const userData = res.locals.user
+    const alreadyInDB = await db.collection("users").findOne({userData})
+          if(alreadyInDB){
+              return res.sendStatus(409)
+    }
     
     try {
         const passwordHash = bcrypt.hashSync(userData.password, parseInt(process.env.DIF));
